429d28a9ba6b80b88158660a14a21e55f69cb5a7
emilee 3 <106194826+emilee1@users.noreply.github.com>
2023-03-15 22:38:01 +0000

    made objects and made it partly work

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ let userLink = document.querySelector(".display-link");
 
 
 // task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
-let imageURL =["pics/uzumaki.jpeg", "pics/hype boy.jpeg", "pics/around the fur.webp", "pics/Jus2.jpg", "pics/flutter - julie.jpeg" ];
-let songNameArray = ["うずまき (Uzumaki)","Hype Boy", "Mascara","Focus On Me", "Flutter"];
-  let songLinkArray = ["https://www.youtube.com/watch?v=fYkShnaf454","https://www.youtube.com/watch?v=ghrlZIMDzbM","https://www.youtube.com/watch?v=XhCBruock0E","https://www.youtube.com/watch?v=uf27Hxrglrk", "https://youtu.be/V9VM1SI1uYw"];
-  let artistArray = ["Machine Girl","New Jeans","Deftones","Jus2", "Julie"]
+//let imageURL =["pics/uzumaki.jpeg", "pics/hype boy.jpeg", "pics/around the fur.webp", "pics/Jus2.jpg", "pics/flutter - julie.jpeg" ];
+//let songNameArray = ["うずまき (Uzumaki)","Hype Boy", "Mascara","Focus On Me", "Flutter"];
+  //let songLinkArray = ["https://www.youtube.com/watch?v=fYkShnaf454","https://www.youtube.com/watch?v=ghrlZIMDzbM","https://www.youtube.com/watch?v=XhCBruock0E","https://www.youtube.com/watch?v=uf27Hxrglrk", "https://youtu.be/V9VM1SI1uYw"];
+  //let artistArray = ["Machine Girl","New Jeans","Deftones","Jus2", "Julie"]
 
 
 
@@ -31,28 +31,42 @@ let songNameArray = ["うずまき (Uzumaki)","Hype Boy", "Mascara","Focus On Me
 // task 11: comment out the arrays data.
 
 // task 12: create an object for each of your songs.
+
 let Uzumaki = {
-  picture = "pics/uzumaki.jpeg",
-  name = "Uzumaki",
-  artist = "Machine Girl",
-  link = "https://www.youtube.com/watch?v=fYkShnaf454"
+  imageURL: "pics/uzumaki.jpeg",
+  songname: "Uzumaki",
+  artist: "Machine Girl",
+  songlink: "https://www.youtube.com/watch?v=fYkShnaf454"
 };
   let hypeBoy = {
-  picture = "pics/hype boy.jpeg",
-  name = "Hype Boy",
-  artist = "New Jeans",
-  link = "https://www.youtube.com/watch?v=ghrlZIMDzbM"
+  imageURL: "pics/hype boy.jpeg",
+  songname: "Hype Boy",
+  artist: "New Jeans",
+  songlink: "https://www.youtube.com/watch?v=ghrlZIMDzbM"
   };
 let Mascara = {
-  picture = "pics/around the fur.webp",
-  name = "Mascara",
-  artist = "Deftones",
-  link = "https://www.youtube.com/watch?v=ghrlZIMDzbM"
+  imageURL: "pics/around the fur.webp",
+  songname: "Mascara",
+  artist: "Deftones",
+  songlink: "https://www.youtube.com/watch?v=ghrlZIMDzbM"
   };
+
+let Focus On Me = {
+  imageURL: "pics/Jus2.jpg",
+  songname: "Focus On Me", 
+  artist: "Jus2",
+  songlink:"https://www.youtube.com/watch?v=uf27Hxrglrk"
+};
+let Flutter = {
+  imageURL: " pics/flutter - julie.jpeg",
+  songname: "Flutter",
+  artist: "Julie",
+  songlink: "https://youtu.be/V9VM1SI1uYw"
+};
 // task 13: inside each object, add key/value pairs to store the image url, song name, artist, and song link.
 
 // task 14: create an array that stores all of the objects.
-
+let Songs = [Uzumaki, hypeBoy, Mascara, Focus On Me, Julie];
 
 
 //REFACTOR LOOPS DAY 
@@ -100,21 +114,9 @@ function emptyDisplay() {
 function displaySongInfo() {
 // task 8: loop through your images array and display the images to your songs in the correct div. Create three more loops. One for the song names, one for the artists, and a last one for the song links.
 
-for (let index = 0; index < imageURL.length; index++){
-let album = imageURL[index];
+for (let index = 0; index < addSongInfo.length; index++){
+let album = addSongInfo[index];
   userURL.insertAdjacentHTML("beforeend", `<img src ="${album}">`);
-}
-for (let i = 0; i < songNameArray.length; i++){
-let songs = songNameArray[i];
-  userSong.insertAdjacentHTML("beforeend", `<p>${songs}</p>`);
-}
-for (let x = 0; x < artistArray.length; x++){
- let artist = artistArray[x];
-  userArtist.insertAdjacentHTML("beforeend",`<p>${artist}</p>`);
-}
-for (let link = 0; link < songLinkArray.length; link++){
-  let song = songLinkArray[link];
-  userLink.insertAdjacentHTML("beforeend",`<a href="${song}">Listen Here!</a>`);
 }
 }
 
